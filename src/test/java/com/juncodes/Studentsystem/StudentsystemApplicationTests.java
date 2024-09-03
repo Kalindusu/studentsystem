851702aecedfddc975036a8ca0cd8ae18ec723a8
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-03 08:40:50 +0530

    frst commit

---
+++ src/test/java/com/juncodes/Studentsystem/StudentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juncodes.Studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
